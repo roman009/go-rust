9ce65ef16b6a1e79e4879d678c482debda13e079
valeriu <valeriu.buzila@gmail.com>
2023-12-08 20:01:54 +0100

    IfNotPresent image policy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-microk8s helm3 upgrade go-main --install --set image.tag=$COMMIT_HASH --set image.pullPolicy=Always --atomic --timeout=1m infrastructure/apps/go-main/ || exit 1
+microk8s helm3 upgrade go-main --install --set image.tag=$COMMIT_HASH --set image.pullPolicy=IfNotPresent --atomic --timeout=1m infrastructure/apps/go-main/ || exit 1
 
-microk8s helm3 upgrade rust-main --install --set image.tag=$COMMIT_HASH --set image.pullPolicy=Always --atomic --timeout=1m infrastructure/apps/rust-main/ || exit 1
+microk8s helm3 upgrade rust-main --install --set image.tag=$COMMIT_HASH --set image.pullPolicy=IfNotPresent --atomic --timeout=1m infrastructure/apps/rust-main/ || exit 1
 
-microk8s helm3 upgrade kill-job --install --set image.tag=$COMMIT_HASH --set image.pullPolicy=Always --atomic --timeout=1m infrastructure/apps/kill-job/ || exit 1
+microk8s helm3 upgrade kill-job --install --set image.tag=$COMMIT_HASH --set image.pullPolicy=IfNotPresent --atomic --timeout=1m infrastructure/apps/kill-job/ || exit 1
