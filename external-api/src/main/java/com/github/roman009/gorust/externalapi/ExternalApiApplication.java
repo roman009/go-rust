81f995e66d740fe7cfa08c3e094e2a0a6b164168
valeriu <valeriu.buzila@gmail.com>
2023-12-16 08:21:17 +0100

    api + ui init

---
+++ external-api/src/main/java/com/github/roman009/gorust/externalapi/ExternalApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.roman009.gorust.externalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalApiApplication.class, args);
+	}
+
+}
---
+++ external-api/src/test/java/com/github/roman009/gorust/externalapi/ExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.roman009.gorust.externalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
